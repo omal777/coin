32b5222ee4c960945a612d8a0ac111ea5f32dedb
omal777 <egorlg3gs@gmail.com>
2022-02-17 13:24:27 +0200

    1.1ver

--- coin.rb
+++ coin.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 puts 'бросаю монетку'
-monetka = rand(10)+1
-if monetka == 10
-  abort "REBRO!"
-elsif monetka <=5
-  puts 'Orel'
-elsif monetka >5
-  puts 'Reshka'
+# Созаю переменную для монеты
+
+# Проверка результата броска
+answ = 'y'
+while answ == 'y'
+  system "clear" or system "cls"
+  monetka = rand(1..10)
+  if monetka == 10
+    puts 'Ребро!'
+  elsif monetka <= 5
+    puts 'Орел'
+  elsif monetka > 5
+    puts 'Решка'
+  end
+  puts 'Еще ?'
+  answ = STDIN.gets.chomp
 end
